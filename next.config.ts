6504cb9856298d616a3bb0cb8b8cda0dc96ff076
duonhungsw <ddhung2003@gmail.com>
2025-07-02 23:06:43 +0700

    deploy6: bỏ qua output

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone", // hoặc bỏ hẳn `output` nếu bạn không cần `export`
   typescript: {
     ignoreBuildErrors: true, // ✅ Bỏ qua lỗi TypeScript khi build
   },
